57fcf1def9535567495b3c6ff6464ad727283f8f
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-02 16:57:59 +0900

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import styles from '../styles/test.module.scss';
 
+const Home = () => {
+  const settings = {
+    // Slickの設定をここに記述
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
   return (
     <div>
-      <h1 className='text-5xl underline'>TOP画面</h1>
-
-
-
+      <h1 className={`${styles.text5xl} ${styles.underline}`}>TOP画面</h1>
       <h2>React Slick Example</h2>
       <Slider {...settings}>
         <div>
@@ -27,38 +31,7 @@ export default function Home() {
         {/* 他のスライドをここに追加 */}
       </Slider>
     </div>
+  );
+};
 
-  )
-}
-
-
-
-// const MySlider = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-//     slidesToScroll: 1
-//   };
-
-//   return (
-//     <div>
-//       <h2>React Slick Example</h2>
-//       <Slider {...settings}>
-//         <div>
-//           <h3>Slide 1</h3>
-//         </div>
-//         <div>
-//           <h3>Slide 2</h3>
-//         </div>
-//         <div>
-//           <h3>Slide 3</h3>
-//         </div>
-//         {/* 他のスライドをここに追加 */}
-//       </Slider>
-//     </div>
-//   );
-// };
-
-
+export default Home;
