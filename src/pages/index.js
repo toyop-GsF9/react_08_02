f7b7ecacab8c7f27bd6ef90eb5b3dfef504afe3a
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-02 16:47:26 +0900

    ええええ

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,3 +14,35 @@ export default function Home() {
 
   )
 }
+
+
+
+const MySlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div>
+      <h2>React Slick Example</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>Slide 1</h3>
+        </div>
+        <div>
+          <h3>Slide 2</h3>
+        </div>
+        <div>
+          <h3>Slide 3</h3>
+        </div>
+        {/* 他のスライドをここに追加 */}
+      </Slider>
+    </div>
+  );
+};
+
+export default MySlider;
