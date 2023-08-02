3129eb1e1b34e84768430168472779e196262cf1
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-02 17:31:02 +0900

    aaa

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
+const withCSS = require('@zeit/next-css');
+module.exports = withCSS({
+  /* ここにオプションが必要な場合は指定します */
+});
 module.exports = nextConfig
