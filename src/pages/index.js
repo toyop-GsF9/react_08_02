ea4620e880c91aaee6e4c1dfcfbb255fe7d9700f
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-02 17:40:30 +0900

    yyyy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from '../styles/test.module.scss';
 
-const Home = () => {
+const home = () => {
   const settings = {
 
     dots: true,
@@ -15,23 +15,22 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1 className={`${styles.text5xl} ${styles.underline}`}>TOP画面</h1>
-      <h2></h2>
-      <Slider {...settings}>
-        <div>
-          <h3>Slide 1</h3>
-        </div>
-        <div>
-          <h3>Slide 2</h3>
-        </div>
-        <div>
-          <h3>Slide 3</h3>
-        </div>
 
-      </Slider>
-    </div>
+
+    <Slider {...settings}>
+      <div>
+        <h3>Slide 1</h3>
+      </div>
+      <div>
+        <h3>Slide 2</h3>
+      </div>
+      <div>
+        <h3>Slide 3</h3>
+      </div>
+
+    </Slider>
+
   );
 };
 
-export default Home;
+export default home;
