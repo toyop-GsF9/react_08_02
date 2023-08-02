89eb8c62f793d7c29889da90d12b08896bee6ced
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-02 17:34:30 +0900

    news

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import style from '../styles/test.module.scss'
-const news = () => {
-	return (
-		<div className='text-5xl'>news
-			<p className={style.test}>テスト</p>
-		</div>
-	)
-}
+import React from "react";
+import style from "../styles/test.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default news
+const news = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
+	return (
+		<Slider {...settings}>
+			<div>
+				<h1 style={style}>1</h1>
+			</div>
+			<div>
+				<h1 style={style}>2</h1>
+			</div>
+			<div>
+				<h1 style={style}>3</h1>
+			</div>
+			<div>
+				<h1 style={style}>4</h1>
+			</div>
+			<div>
+				<h1 style={style}>5</h1>
+			</div>
+		</Slider>
+	);
+};
+
+export default news;
